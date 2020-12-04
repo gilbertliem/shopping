137b656afa7e6c51393d0807fa52f71756be6f66
gilbert yohanes <yohanesbert97@gmail.com>
2020-12-04 10:43:18 +0700

    add picture in assets

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Body.module.css";
+import image1 from "../Assets/Images/p1.jpg";
 
 // ---------- //
 
@@ -7,12 +8,19 @@ function Body() {
   return (
     <>
       <div className={styles.body}>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-          <li>Product 4</li>
-        </ul>
+        <div className={styles.card}>
+          <a href="#">
+            <img src={image1} alt="product" />
+          </a>
+          <div className={styles.cardBody}>
+            <a href="#">
+              <div>Nike Slim Shirt</div>
+            </a>
+            <div className={styles.rating}>
+              <span>{/* <i className={}></i> */}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
