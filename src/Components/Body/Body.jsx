9702a9758ca00f848e21279ed31907aa47f0b281
gilbert yohanes <yohanesbert97@gmail.com>
2020-12-30 14:49:05 +0700

    add cards

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,24 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Body.module.css";
 import image1 from "../Assets/Images/p1.jpg";
+import star from "../Assets/Icons/star.png";
+import Rating from "@material-ui/lab/Rating";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 // ---------- //
 
 function Body() {
+  const [value, setValue] = useState(0);
+
   return (
     <>
       <div className={styles.body}>
         <div className={styles.card}>
           <a href="#">
-            <img src={image1} alt="product" />
+            <img src={image1} alt="product" className={styles.image} />
           </a>
           <div className={styles.cardBody}>
-            <a href="#">
-              <div>Nike Slim Shirt</div>
+            <a href="#" className={styles.namelink}>
+              <div className={styles.name}>Nike Slim Shirt</div>
             </a>
             <div className={styles.rating}>
-              <span>{/* <i className={}></i> */}</span>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
             </div>
+            <div className={styles.price}>$120</div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <a href="#">
+            <img src={image1} alt="product" className={styles.image} />
+          </a>
+          <div className={styles.cardBody}>
+            <a href="#" className={styles.namelink}>
+              <div className={styles.name}>Nike Slim Shirt</div>
+            </a>
+            <div className={styles.rating}>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+            <div className={styles.price}>$120</div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <a href="#">
+            <img src={image1} alt="product" className={styles.image} />
+          </a>
+          <div className={styles.cardBody}>
+            <a href="#" className={styles.namelink}>
+              <div className={styles.name}>Nike Slim Shirt</div>
+            </a>
+            <div className={styles.rating}>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+            <div className={styles.price}>$120</div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <a href="#">
+            <img src={image1} alt="product" className={styles.image} />
+          </a>
+          <div className={styles.cardBody}>
+            <a href="#" className={styles.namelink}>
+              <div className={styles.name}>Nike Slim Shirt</div>
+            </a>
+            <div className={styles.rating}>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+            <div className={styles.price}>$120</div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <a href="#">
+            <img src={image1} alt="product" className={styles.image} />
+          </a>
+          <div className={styles.cardBody}>
+            <a href="#" className={styles.namelink}>
+              <div className={styles.name}>Nike Slim Shirt</div>
+            </a>
+            <div className={styles.rating}>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+            <div className={styles.price}>$120</div>
           </div>
         </div>
       </div>
