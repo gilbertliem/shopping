be90738a7c8ccd1e54edd3633fcba7e64d8a8864
gilbert yohanes <yohanesbert97@gmail.com>
2021-01-01 20:44:19 +0700

    change static to dinamic

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import styles from "./Body.module.css";
-import image1 from "../Assets/Images/p1.jpg";
-import star from "../Assets/Icons/star.png";
 import Rating from "@material-ui/lab/Rating";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import data from "../../data";
+// import image1 from "../Assets/Images/p1.jpg";
+// import star from "../Assets/Icons/star.png";
+// import Typography from "@material-ui/core/Typography";
+// import Box from "@material-ui/core/Box";
 
 // ---------- //
 
@@ -14,106 +15,35 @@ function Body() {
   return (
     <>
       <div className={styles.body}>
-        <div className={styles.card}>
-          <a href="#">
-            <img src={image1} alt="product" className={styles.image} />
-          </a>
-          <div className={styles.cardBody}>
-            <a href="#" className={styles.namelink}>
-              <div className={styles.name}>Nike Slim Shirt</div>
-            </a>
-            <div className={styles.rating}>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
+        {data.products.map((product) => {
+          console.log(product.image);
+          return (
+            <div className={styles.card} key={product.id}>
+              <a href={`/product/${product.id}`}>
+                <img
+                  src={product.image}
+                  alt={product.image}
+                  className={styles.image}
+                />
+              </a>
+              <div className={styles.cardBody}>
+                <a href={`/product/${product.id}`} className={styles.namelink}>
+                  <div className={styles.name}>{product.name}</div>
+                </a>
+                <div className={styles.rating}>
+                  <Rating
+                    name="simple-controlled"
+                    value={product.rating}
+                    onChange={(event, newValue) => {
+                      setValue(newValue);
+                    }}
+                  />
+                </div>
+                <div className={styles.price}>${product.price}</div>
+              </div>
             </div>
-            <div className={styles.price}>$120</div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <a href="#">
-            <img src={image1} alt="product" className={styles.image} />
-          </a>
-          <div className={styles.cardBody}>
-            <a href="#" className={styles.namelink}>
-              <div className={styles.name}>Nike Slim Shirt</div>
-            </a>
-            <div className={styles.rating}>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </div>
-            <div className={styles.price}>$120</div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <a href="#">
-            <img src={image1} alt="product" className={styles.image} />
-          </a>
-          <div className={styles.cardBody}>
-            <a href="#" className={styles.namelink}>
-              <div className={styles.name}>Nike Slim Shirt</div>
-            </a>
-            <div className={styles.rating}>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </div>
-            <div className={styles.price}>$120</div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <a href="#">
-            <img src={image1} alt="product" className={styles.image} />
-          </a>
-          <div className={styles.cardBody}>
-            <a href="#" className={styles.namelink}>
-              <div className={styles.name}>Nike Slim Shirt</div>
-            </a>
-            <div className={styles.rating}>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </div>
-            <div className={styles.price}>$120</div>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <a href="#">
-            <img src={image1} alt="product" className={styles.image} />
-          </a>
-          <div className={styles.cardBody}>
-            <a href="#" className={styles.namelink}>
-              <div className={styles.name}>Nike Slim Shirt</div>
-            </a>
-            <div className={styles.rating}>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </div>
-            <div className={styles.price}>$120</div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
